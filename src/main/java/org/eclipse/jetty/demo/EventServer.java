c3d02b242827c1dfafd9e2df4f89d3261420d687
ztlikejian <ztlikejain@126.com>
2015-09-06 17:08:37 +0800

    add WebsocketServerJetty9

---
+++ src/main/java/org/eclipse/jetty/demo/EventServer.java
@@ -0,0 +1,54 @@
+package org.eclipse.jetty.demo;
+
+import javax.servlet.ServletException;
+import javax.websocket.server.ServerContainer;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.websocket.jsr356.server.deploy.WebSocketServerContainerInitializer;
+
+import com.likejian.thread.ChatThread;
+import com.likejian.thread.SendPingThread;
+
+public class EventServer {
+	
+	public static void main(String[] args) {
+		try {
+			init();
+			
+			Server server = new Server();
+			ServerConnector connector = new ServerConnector(server);
+			connector.setPort(8081);
+			server.addConnector(connector);
+
+			// Setup the basic application "context" for this application at "/"
+			// This is also known as the handler tree (in jetty speak)
+			ServletContextHandler context = new ServletContextHandler(
+					ServletContextHandler.SESSIONS);
+			context.setContextPath("/WebsocketServerJetty9");
+			server.setHandler(context);
+			
+			// Initialize javax.websocket layer
+			ServerContainer wscontainer = WebSocketServerContainerInitializer
+					.configureContext(context);
+
+			// Add WebSocket endpoint to javax.websocket layer
+			wscontainer.addEndpoint(EventSocket.class);
+
+			server.start();
+			server.dump(System.err);
+			server.join();
+		} catch (Throwable t) {
+			t.printStackTrace(System.err);
+		}
+	}
+	
+	public static void init() throws ServletException {
+		ChatThread chatThread = new ChatThread();
+		chatThread.setDaemon(true);
+		chatThread.start();
+		
+		new SendPingThread().start();
+	}
+}
---
+++ src/main/java/org/eclipse/jetty/demo/EventSocket.java
@@ -0,0 +1,59 @@
+package org.eclipse.jetty.demo;
+import java.io.IOException;
+
+import javax.websocket.ClientEndpoint;
+import javax.websocket.CloseReason;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+import org.apache.log4j.Logger;
+
+import com.likejian.thread.ChatThread;
+
+@ClientEndpoint
+@ServerEndpoint(value="/websocket/")
+public class EventSocket
+{
+private static Logger logger = Logger.getLogger(EventSocket.class.getName());
+	
+	@OnOpen
+	public void onOpen(Session session) {
+		String nick = (String)session.getRequestParameterMap().get("nick").get(0);
+		//是否可以在这里进行一个用户合法性的验证
+		logger.debug("Client connected sessionId is " + session.getId());
+		logger.debug("nick = " + nick);
+		session.getUserProperties().put("nick", nick);
+		//session.setMaxIdleTimeout(2000);		//连接最大空闲时间，也就是连接多久没用就会被干掉		
+		ChatThread.sessionSet.add(session);
+		String content = "["+ nick+"]进入聊天室";
+		ChatThread.contentQueue.add(content);
+	}
+	
+	@OnMessage
+	public void onMessage(String message, Session session) throws IOException,
+			InterruptedException {
+		logger.debug("Received: [" + message + "] from sessionId=" + session.getId());
+		String nick = (String)session.getUserProperties().get("nick");
+		String content = "["+ nick +"]说:" + message;
+		ChatThread.contentQueue.add(content);
+	}
+
+	@OnClose
+	public void onClose(Session session, CloseReason reason) {
+		logger.debug("Connection closed sessionId is " + session.getId());
+		String nick = (String)session.getUserProperties().get("nick");
+		String content = "["+ nick +"]退出聊天室";
+		ChatThread.contentQueue.add(content);
+		ChatThread.sessionSet.remove(session);
+	}
+
+	@OnError
+	public void onError(Session session, Throwable t) {
+		t.printStackTrace();
+		logger.debug("System Error " + session.getId());
+	}
+}
